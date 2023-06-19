92086bcfdd268286ac4ad580b00d2fe33da6bdef
INDAH-780 <mbahindah780@gmail.com>
2023-06-19 22:46:31 +0100

    all

---
+++ backendPortfolio/config/app.config.js
@@ -0,0 +1,8 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+  DB: "mongodb+srv://mbahindah780:HYIQPhPvAsN8e8Yp@portfoliobackend.nvcfqqt.mongodb.net/ReactBackendProject?retryWrites=true&w=majority",
+};
+module.exports = {
+  MONGO_DB_CONFIG
+};
